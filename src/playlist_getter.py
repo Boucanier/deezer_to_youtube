cf3060d1098d4e447c183f36df23682fc3c44274
boucanier <chironjules.78@gmail.com>
2023-04-18 21:07:56 +0200

    Add deezer_menu() to ask the user the playlist he wants to convert

--- src/playlist_getter.py
+++ src/playlist_getter.py
@@ -14,9 +14,24 @@ def get_playlists(user_id : str) :
         data = response.json()
         [title_list.append(playlist["title"]) for playlist in data["data"]]
         [id_list.append(playlist["id"]) for playlist in data["data"]]
-        print(title_list)
-        print(id_list)
     else:
         print('Error:', response.status_code, response.reason)
     
     return title_list, id_list
+
+
+def deezer_menu(title_list, id_list):
+    for i in range(len(title_list)) :
+        print(i, title_list[i])
+    
+    choice = -1
+    while choice not in range(len(id_list)) :
+        choice = input("Choice : ")
+        if choice.isdigit():
+            choice = int(choice)
+        else :
+            choice = -1
+    
+    playlist_id = id_list[choice]
+    
+    return playlist_id
