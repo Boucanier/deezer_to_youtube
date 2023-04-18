aa0d36dac3842f39c189ad0f6f4a4401cb4b6656
boucanier <chironjules.78@gmail.com>
2023-04-18 20:56:08 +0200

    Add get_playlists to get playlists info from a user_id

--- src/playlist_getter.py
+++ src/playlist_getter.py
@@ -1,2 +1,22 @@
 import requests
 
+def get_playlists(user_id : str) :
+
+    title_list = []
+
+    id_list = []
+
+    url = "https://api.deezer.com/user/" + user_id + "/playlists"
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        [title_list.append(playlist["title"]) for playlist in data["data"]]
+        [id_list.append(playlist["id"]) for playlist in data["data"]]
+        print(title_list)
+        print(id_list)
+    else:
+        print('Error:', response.status_code, response.reason)
+    
+    return title_list, id_list
