4d81c992ab3c6331a7abcbd058f851c9d20e885b
boucanier <chironjules.78@gmail.com>
2023-04-25 16:46:37 +0200

    Bash installation script

---
+++ installation.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install python3
+pip install requests
+pip install google-api-python-client
+pip google-auth-oauthlib
+pip google-auth-httplib2
+clear
+echo "Everything is installed"
+sleep 1
+echo ...
+sleep 1
+echo "The program is ready to run"
+sleep 2
+clear
