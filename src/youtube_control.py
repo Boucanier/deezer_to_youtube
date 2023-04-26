3736679a3ba9a656c4fbe1bfcb62f813a472ea9c
boucanier <chironjules.78@gmail.com>
2023-04-26 12:31:02 +0200

    function to get tracks based on csv file

--- src/youtube_control.py
+++ src/youtube_control.py
@@ -1,14 +1,9 @@
-# -*- coding: utf-8 -*-
-
-# Sample Python code for youtube.channels.list
-# See instructions for running these code samples locally:
-# https://developers.google.com/explorer-help/code-samples#python
-
 import os
 
 import google_auth_oauthlib.flow
 import googleapiclient.discovery
 import googleapiclient.errors
+import csv
 
 scopes = ["https://www.googleapis.com/auth/youtube.readonly", "https://www.googleapis.com/auth/youtube.force-ssl"]
 
@@ -32,6 +27,15 @@ def create_playlist(youtube, title, description):
     return response['id']
 
 
+def search_tracks():
+    tracks = {}
+    with open("data/tracks.csv", "r") as track_file :
+        spamreader = csv.reader(track_file, delimiter=',')
+        for e in spamreader :
+            tracks[e[0]] = e[1]
+    return tracks
+
+
 def main():
     # Disable OAuthlib's HTTPS verification when running locally.
     # *DO NOT* leave this option enabled in production.
@@ -42,13 +46,12 @@ def main():
     client_secrets_file = "data/client_secret_id.json"
 
     # Get credentials and create an API client
-    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-        client_secrets_file, scopes)
+    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(client_secrets_file, scopes)
     credentials = flow.run_local_server(port = 0)
-    youtube = googleapiclient.discovery.build(
-        api_service_name, api_version, credentials=credentials)
+    youtube = googleapiclient.discovery.build(api_service_name, api_version, credentials=credentials)
 
-    create_playlist(youtube, "deezer_to_youtube", "Here is a copy of your deezer playlist")
+    # id = create_playlist(youtube, "deezer_to_youtube", "Here is a copy of your deezer playlist")
+    tracks = search_tracks()
 
 if __name__ == "__main__":
     main()
