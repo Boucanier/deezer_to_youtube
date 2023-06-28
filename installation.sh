3834df8468fe0f5a8b98a837ed6725c5cae6cbff
boucanier <chironjules.78@gmail.com>
2023-06-29 00:45:11 +0200

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -2,10 +2,7 @@
 
 sudo apt update
 sudo apt install python3
-pip install requests
-pip install google-api-python-client
-pip google-auth-oauthlib
-pip google-auth-httplib2
+pip install requests install google-api-python-client google-auth-oauthlib google-auth-httplib2
 clear
 echo "Everything is installed"
 sleep 1
